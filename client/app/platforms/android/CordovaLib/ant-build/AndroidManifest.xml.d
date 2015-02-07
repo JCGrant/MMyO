e037724174a2238c9445428f6512ec4c8916b372
JCGrant <jamescolin.grant@gmail.com>
2015-02-07 20:17:03 +0000

    Revert "Added bitbump code."
    This reverts commit 5f670d7a689497fe1e1cfedd4abef6c46ea3c3b1.

--- client/app/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/nkp/projects/bitbump/android/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/nkp/projects/bitbump/android/platforms/android/CordovaLib/AndroidManifest.xml \
--- client/app/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/nkp/projects/bitbump/android/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/nkp/projects/bitbump/android/platforms/android/AndroidManifest.xml \
